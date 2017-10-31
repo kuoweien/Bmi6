429659f43b05aa4fa1d7914a6e1ab1ef27e62fc1
Kuoweien <aasdfangela@gmail.com>
2017-10-31 12:12:41 +0800

    initial commit

---
+++ app/src/main/java/com/wayne/bmi6/MainActivity.java
@@ -0,0 +1,27 @@
+package com.wayne.bmi6;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void bmi(View view){
+        EditText edweight = (EditText)findViewById(R.id.ed_weight);
+        EditText edheight = (EditText)findViewById(R.id.ed_height);
+        float weight = Float.parseFloat(edweight.getText().toString());
+        float height = Float.parseFloat(edheight.getText().toString());
+        float bmi = weight / (height*height);
+        new AlertDialog.Builder(this)
+                .setMessage("BMI" +bmi)
+                .setPositiveButton("OK",null)
+                .show();
+    }
+}
